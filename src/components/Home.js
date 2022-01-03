5b67e265a2e6e9ebbfc03495f1f9594ad64a6d31
gugohh <oohhgug@gmail.com>
2022-01-03 17:50:43 +0100

    working on my searchstyle component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import colors from '../assets/colors'
 import styled from "styled-components";
+import Searchstyle from './styled/Searchstyle';
 
-
-
+const div = styled.div`
+background-color:${colors.Primary};
+`
+const Title = styled.h1`
+  font-family: "DM Mono";
+  font-weight: normal;
+  font-size: 1.5rem;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
 
 
 const Home = () => {
     return (
         <div>
-            
+            <Title>HoHo-NFT</Title>
+            <Searchstyle/>
         </div>
     )
 }
