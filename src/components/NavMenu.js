52e61068d45d88e57994d8699f1b25f8bf1325ea
gugohh <oohhgug@gmail.com>
2022-01-03 14:47:44 +0100

    working on our NavMenu component

---
+++ src/components/NavMenu.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import {FiHome} from 'react-icons/fi'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiStore} from 'react-icons/bi'
+import colors from '../assets/colors'
+import { useLocation,useNavigate } from "react-router";
+
+const NavMneuEl = styled.nav`
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.01);
+  position: absolute;
+  bottom: 0;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 10%;
+  backdrop-filter: blur(50px);
+`;
+
+import React from 'react'
+
+const NavMenu = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavMenu
