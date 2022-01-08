7b5a9a0d1702721b158b434d0943a0aa92420052
gugohh <oohhgug@gmail.com>
2022-01-08 13:47:31 +0100

    Working on featuredTab component

--- src/components/FeaturedTab.js
+++ src/components/FeaturedTab.js
@@ -12,12 +12,59 @@ import image4  from '../assets/img/image4.png'
 import Img from "./styled/imgstyles";
 
 const FeaturedTabEl = styled.div`
-  display: grid;
+  display: grid; 
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
   position: relative;
   overflow: hidden;
   height: 400px;
+`;
+
+const Item = styled.div`
+  background-color: ${Colors.CardBackground};
+  padding: 0 0.4rem;
+  border-radius: 12px;
+  display: flex;
+  cursor: pointer;
+  flex-direction: column;
+`;
+
+const Title = styled.span`
+  font-size: 1.1rem;
+  margin-top: 0.5rem;
+`;
+
+const SubTitle = styled.span`
+  color: ${Colors.Gray};
+  font-size: 1rem;
+`;
+
+const ShowMore = styled.span`
+  text-align: center;
+  position: absolute;
+  cursor: pointer;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  font-weight: 500;
+  background: rgba(255, 255, 255, 0.32);
+  padding: 1rem;
+  border: 1px solid ${Colors.GrayBorder};
+  backdrop-filter: blur(17px);
+  border-radius: 12px;
+`;
+
+const BottomFade = styled.span`
+  text-align: center;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 25%;
+  background-image: linear-gradient(
+    rgba(255, 255, 255, 0),
+    ${Colors.Background}
+  );
 `;
 
 const FeaturedTab = () => {
